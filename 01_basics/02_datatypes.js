61af9063253524d25f28ab2f48a30885093af8df
pucchkii <147378380+pucchkii@users.noreply.github.com>
2025-06-20 05:41:13 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs not browser
+
+console.log(3+3) //code reusability
+console.log("amit")
+
+let name="amit"
+let age=18
+let loggedIn=true
+
+//datatypes
+// number =>2 power 53
+// string => ""
+// boolean =>true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof null) // null is an object
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof loggedIn)
+console.log(typeof undefined) // undefined
