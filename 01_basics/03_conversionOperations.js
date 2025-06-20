8ef0528d6a7c31028e4d84ca13cc163e703008c7
pucchkii <147378380+pucchkii@users.noreply.github.com>
2025-06-20 05:56:39 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,24 @@
+let score="node33abc"
+//console.log(typeof(score));
+
+let valueToNumber = Number(score);
+//console.log(typeof(valueToNumber));
+//console.log(valueToNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// "true" => 1
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0=>false;
+// "" => false
+// "amit" =>true
+
+let someNumber = 33;
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
