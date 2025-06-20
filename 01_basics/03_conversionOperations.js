fc45ea6fda5276643da406299f2ab9c2bb5cbba8
pucchkii <147378380+pucchkii@users.noreply.github.com>
2025-06-20 06:10:07 +0000

    conversionOperation

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -12,7 +12,7 @@ let valueToNumber = Number(score);
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 // 1 => true; 0=>false;
 // "" => false
@@ -20,5 +20,35 @@ console.log(booleanIsLoggedIn)
 
 let someNumber = 33;
 let stringNumber=String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+// ***********************************  Operations  *************************************//
+
+let value=3
+let negValue=-value
+//console.log(negValue)
+
+let str1 = "Amit"
+let str2 = " Yadav"
+
+let str3 = str1 + str2
+console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
